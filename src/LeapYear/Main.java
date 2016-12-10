6e169b98fac4b00f5dcbbbd7f12ebc58a49bdddf
MurphysL <1619673065@qq.com>
2016-12-10 12:17:12 +0800

    string01

--- src/LeapYear/Main.java
+++ src/LeapYear/Main.java
@@ -3,7 +3,15 @@ package LeapYear;
 import java.util.Scanner;
 
 /**
- * Created by lenovo on 2016/12/10.
+ * 给定一个年份，判断这一年是不是闰年。
+ *
+ * 当以下情况之一满足时，这一年是闰年：
+ *
+ * 1. 年份是4的倍数而不是100的倍数；
+ *
+ * 2. 年份是400的倍数。
+ *
+ * 其他的年份都不是闰年。
  */
 public class Main {
 
