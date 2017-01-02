0687b51d90859ecbc022bedc56e47845ca886191
MurphysL <1619673065@qq.com>
2017-01-03 00:06:01 +0800

    ThreeSort

--- src/ThreeSort/Main.java
+++ src/ThreeSort/Main.java
@@ -1,27 +1,34 @@
 package ThreeSort;
 
-import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
- * Created by lenovo on 2016/12/16.
+ *  编写一个程序，输入3个整数，然后程序将对这三个整数按照从大到小进行排列。
+ *　输入格式：输入只有一行，即三个整数，中间用空格隔开。
  */
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        BigInteger[] bi = new BigInteger[3];
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        long c = scanner.nextLong();
 
-        bi[0] = scanner.nextBigInteger();
-        bi[1] = scanner.nextBigInteger();
-        bi[2] = scanner.nextBigInteger();
+        if(a < b){
+            long t = a;
+            a = b;
+            b = t;
+        }
+        if(a < c){
+            long t = a;
+            a = c;
+            c = t;
+        }
+        if(b < c){
+            long t = b;
+            b = c;
+            c = t;
+        }
 
-
-//        for(int i = 0 ;i < 3;i ++){
-//            for(int j = 1 ; j < 3 - i ;j ++){
-//                if(bi[j].){
-//
-//                }
-//            }
-//        }
+        System.out.print(a + " " + b + " " + c);
     }
 }
